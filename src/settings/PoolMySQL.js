ff9e7422effe4178c96c578382a7228aeceeae1f
Olaf Gonzalez Cortes <olaf.gonzalez1309@gmail.com>
2021-05-28 18:08:51 -0500

    Fix

--- src/settings/PoolMySQL.js
+++ src/settings/PoolMySQL.js
@@ -7,8 +7,7 @@ var pool  = mysql.createPool({
   user:     process.env.MYSQL_USER,
   port:     process.env.MYSQL_PORT,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  timezone:'-02:00'
+  database: process.env.MYSQL_DATABASE
 });
 
 module.exports = pool
